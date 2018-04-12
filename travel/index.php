e3fcf8a125b3514900f907d657e27671bd1e80a2
khvichalagvilava <xvichiko111@yandex.ru>
2018-04-12 15:40:01 +0400

    designe changes

---
+++ travel/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Travel</title>
+
+
+ <link rel="stylesheet" type="text/css" href="css/css.css">
+
+
+</head>
+<body>
+  
+  
+<footer class="navigation">
+  <img src="https://cdn4.iconfinder.com/data/icons/travel-2/500/earth_globe_map_flight_fly_base_coordinate_coordinates_direction_find_travel-512.png" alt="">
+  <ul>
+    <li class="meny"><a href="">მთავარი გვერდი</a></li>
+    <li class="meny"><a href="">ერთდღიანი ტურები</a></li>
+    <li class="meny"><a href="">ერთკვირიანი ტურები</a></li>
+    <li class="meny"><a href="">ჩვენ შესახებ</a></li>
+    <li class="meny"><a href="">კონტაკტები</a></li>
+  </ul>
+  
+  
+  
+</footer>
+  
+  
+  <section class="sectionslaid">
+    <img src="http://www.nadia-tour.com/UserFiles/resort-gallery/148846088758b81c57c8e95.jpg" alt="">
+    
+    <div class="slaid"></div>
+    
+    
+  </section>
+  
+  <section class="table">
+    <div class="divtable"></div>
+    <div class="divtable"></div>
+    <div class="divtable"></div>
+    <div class="divtable"></div>
+    <div class="divtable"></div>
+    <div class="divtable"></div>
+    
+    
+    
+    
+    
+  </section>
+  
+  
+  
+  
+  
+  
+  
+</body>
+</html>
